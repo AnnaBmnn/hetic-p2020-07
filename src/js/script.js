1be004ee08db1b3f57e425fc9141b88fd8044a3e
MinDBreaK <picoo1@bbox.fr>
2017-11-16 20:29:46 +0100

    :lipstick: Update the home page and style files for yellow

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,11 @@
 let canvas = document.querySelector("canvas");
 let context = canvas.getContext("2d");
 var coords = {x: 0, y: 200, x_2: 0, y_2: 200 , x_3: 0, y_3: 200};
+var direct = {ball1: 1, ball2: 1, ball3: -1};
 
-context.canvas.width = document.body.clientWidth;
-context.canvas.heigth = document.body.clientHeigth;
+context.canvas.width = document.getElementById("yellow").clientWidth;
+context.canvas.height = document.getElementById("yellow").clientHeight;
+
 // Créé la variable image
 let image = new Image();
 
@@ -13,30 +15,31 @@ function loop()
 {
     window.requestAnimationFrame(loop);
     // Mise à jour des coordonnées
-    coords.x += 2;
-    coords.x_2 += 4;
-    coords.x_3 -= 3;
-    coords.y = (canvas.height - 114) - Math.abs(Math.cos(+new Date() / 300)) * 500;
-    coords.y_2 = (canvas.height - 114) - Math.abs(Math.cos(+new Date() / 500)) * 200;
-    coords.y_3 = (canvas.height - 114) - Math.abs(Math.cos(+new Date() / 400)) * 300;
+    coords.x += 2 * direct.ball1;
+    coords.x_2 += 4 * direct.ball2;
+    coords.x_3 += 3 * direct.ball3;
+    coords.y = (canvas.height - (80 * window.devicePixelRatio)) - Math.abs(Math.cos(+new Date() / 300)) * 300;
+    coords.y_2 = (canvas.height - (80 * window.devicePixelRatio)) - Math.abs(Math.cos(+new Date() / 500)) * 200;
+    coords.y_3 = (canvas.height - (80 * window.devicePixelRatio)) - Math.abs(Math.cos(+new Date() / 400)) * 300;
     context.clearRect(0, 0, canvas.width, canvas.height);
     // Limite
-    if (coords.x > canvas.width + 50) {
-        coords.x = -50;
+    if ((coords.x > canvas.width - (80 * window.devicePixelRatio) && direct.ball1 === 1)
+        || (coords.x < 0 && direct.ball1 === -1)) {
+        direct.ball1 *= -1;
     }
     // Limite
-    if (coords.x_2 > canvas.width + 50) {
-        coords.x_2 = -50;
+    if ((coords.x_2 > canvas.width - (80 * window.devicePixelRatio) && direct.ball2 === 1) || (coords.x_2 < 0 && direct.ball2 === -1)) {
+        direct.ball2 *= -1;
     }
 
-    if (coords.x_3 < -50) {
-        coords.x_3 = canvas.width + 50;
+    if ((coords.x_3 > canvas.width - (80 * window.devicePixelRatio)  && direct.ball3 === 1) || (coords.x_3 < 0 && direct.ball3 === -1)) {
+        direct.ball3 *= -1;
     }
 
     // Dessin de la balle
-    context.drawImage(image, coords.x, coords.y, 90, 80);
-    context.drawImage(image, coords.x_2 - 100, coords.y_2, 90, 80);
-    context.drawImage(image, coords.x_3 - 100, coords.y_3, 90, 80);
+    context.drawImage(image, coords.x, coords.y, 90 * window.devicePixelRatio, 80 * window.devicePixelRatio);
+    context.drawImage(image, coords.x_2, coords.y_2, 90 * window.devicePixelRatio, 80 * window.devicePixelRatio);
+    context.drawImage(image, coords.x_3, coords.y_3, 90 * window.devicePixelRatio, 80 * window.devicePixelRatio);
 
 }
 
