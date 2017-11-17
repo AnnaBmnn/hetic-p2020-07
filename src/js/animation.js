23f97cea0da7fcbd8e1fc3087313733fbf7e8a78
AnnaBmnn <annabaumann@laposte.net>
2017-11-17 14:16:59 +0100

    :bug: fix bug flacon parfum

--- src/js/animation.js
+++ src/js/animation.js
@@ -8,14 +8,14 @@ class Paralax {
     constructor( divParalax){
         this.div = divParalax
         this.speed = this.div.dataset.speed*0.1
-        this.breakpoint = this.div.offsetTop
+        this.position = this.div.offsetTop
     }
     //animate(scrollY): scrollY is the window.scrollY
     //this method create the paralax effect
     animate(scrollY){
             //distScrollY is the amount of pixel the div has to move
-            //we have to do scrollY-thi.breakpoint so that the div donc move to much
-            const distScrollY = (scrollY-this.breakpoint)*this.speed
+            //we have to do scrollY-thi.position so that the div donc move to much
+            let distScrollY = (scrollY-this.position)*this.speed
             this.div.style.transform = `translate3D(0, ${distScrollY}px, 0)`            
     }
 }
